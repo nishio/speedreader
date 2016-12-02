4c4cd33ebf9c8a3b77681baac4533a5204691e1f
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2016-12-02 17:45:51 +0900

    add files

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+mkdir tmp\speedreader
+copy .\Deploy\Speedreader.exe .\tmp\speedreader
+mkdir .\tmp\speedreader\Engine
+xcopy '.\Speedreader\Engine\dll(x86)' .\tmp\speedreader\Engine
+copy .\Speedreader\speedreader.ini .\tmp\speedreader
+xcopy .\Speedreader\doc .\tmp\speedreader
+cd tmp
+"C:\Program Files\7-Zip\7z.exe" a -r speedreader.zip speedreader
+copy speedreader.zip c:\Users\nishio\Desktop
+
+
+
