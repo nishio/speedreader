41a5a8efa1c92e18d66a9142d5b85221c98f00c7
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2016-12-14 17:37:40 +0900

    fix bug

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ copy .\Deploy\Speedreader.exe .\tmp\speedreader
 mkdir .\tmp\speedreader\Engine
 xcopy '.\Speedreader\Engine\dll(x86)' .\tmp\speedreader\Engine
 copy .\Speedreader\speedreader.ini .\tmp\speedreader
-xcopy .\Speedreader\doc .\tmp\speedreader
+xcopy .\Speedreader\speedreader\doc .\tmp\speedreader
 cd tmp
 "C:\Program Files\7-Zip\7z.exe" a -r speedreader.zip speedreader
 copy speedreader.zip c:\Users\nishio\Desktop
